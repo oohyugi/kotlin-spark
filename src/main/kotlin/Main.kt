cb92fb9aa82b14c3aa37f2182d87afd04f6f4908
oohyugi <yogi@gits.co.id>
2017-08-03 18:36:37 +0700

    upload to heroku

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
 import spark.Spark.*
 
-fun main(args : Array<String>){
-    port(9091)
-    Router().run()
 
+object Main {
+    @JvmStatic fun main(args: Array<String>) {
+        port(getHerokuAssignedPort())
+        Router().run()
+    }
 
+    fun getHerokuAssignedPort(): Int {
+        val processBuilder = ProcessBuilder()
+        if (processBuilder.environment()["PORT"] != null) {
+            return Integer.parseInt(processBuilder.environment()["PORT"])
+        }
+        return 4567 //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
